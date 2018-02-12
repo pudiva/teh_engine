c33628df460441ab4181694962c5c0c6e6ad5eb0
dota? =op <rogi@skylittlesystem.org>
2018-02-12 05:26:43 -0200

    Transforma teh_model em lista de tri.

--- sauce/teh_bspc.c
+++ sauce/teh_bspc.c
@@ -2,9 +2,40 @@
 
 #include "teh_model.h"
 #include "bspc.h"
+#include "vec.h"
 
 struct teh_model in;
 struct teh_model out;
+struct node* node;
+
+struct tri* list;
+
+void make_tris()
+{
+	int i, j;
+	struct tri* x;
+	float e1[3], e2[3];
+
+	for (i = 0; i < in.triangle_c; ++i)
+	{
+		x = tri_alloc();
+
+		for (j = 0; j < 3; ++j)
+		{
+			vec3_copy(in.vertex_v[3*i+j], x->v[j]);
+			vec2_copy(in.tex_coord_v[3*i+j], x->tc[j]);
+		}
+
+		vec3_copy(x->v[1], e1);
+		vec3_axpy(-1, x->v[0], e1);
+
+		vec3_copy(x->v[2], e2);
+		vec3_axpy(-1, x->v[0], e2);
+
+		vec3_cross(x->p, e1, e2);
+		vec3_normalize(x->p);
+	}
+}
 
 int main(int argc, char *argv[])
 {
@@ -12,6 +43,10 @@ int main(int argc, char *argv[])
 	read_teh_model_vertices(&in, argv[1]);
 	assert (0 < in.triangle_c);
 	assert (0 < in.frame_c);
+
+	node = bspc(list);
+
+	assert (node);
 
 	free_teh_model(&in);
 	return 0;
